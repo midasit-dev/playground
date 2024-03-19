07a2ef79a01e002250d9162e6f3d7e0819406d9d
lhy0118 <lhy0118@midasit.com>
2024-03-19 19:39:36 +0900

    불필요한 Home 폴더 삭제 및 App.tsx에 Layout폴더 index.tsx import 하도록 변경

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import AppKit from './App';
+import App from './App';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <AppKit />
+    <App />
   </React.StrictMode>
 );
