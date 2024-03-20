8f514625ae552accc01375cdc8475ff6e0b2cf59
lhy0118 <lhy0118@midasit.com>
2024-03-20 12:13:46 +0900

    SnackbarProvider 설정 반영

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
-import * as React from 'react';
-import Layout from './Layout/index'; 
-import { RecoilRoot } from 'recoil';
+import * as React from "react";
+import Layout from "./Layout/index";
+import { RecoilRoot } from "recoil";
+import { SnackbarProvider, closeSnackbar } from "notistack";
+import { 
+  IconButton, 
+  Icon
+ } from '@midasit-dev/moaui';
+ 
 export default function App() {
   return (
     <RecoilRoot>
-      <Layout />
+      <SnackbarProvider
+        maxSnack={3}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "center",
+        }}
+        action={(key) => (
+          <IconButton
+            transparent
+            transparentColor="white"
+            onClick={() => closeSnackbar(key)}
+          >
+            <Icon iconName="Close" />
+          </IconButton>
+        )}
+      >
+        <Layout />
+      </SnackbarProvider>
     </RecoilRoot>
   );
 }
