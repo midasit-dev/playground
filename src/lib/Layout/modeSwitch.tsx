2037cdd5905da29d2c1afde4c9028df3bb8a4e1c
lhy0118 <lhy0118@midasit.com>
2024-04-12 14:40:39 +0900

    Mode 버튼 아이콘 수정 및 Outlined 아이콘 추가 적용
    Componentized 아이콘 - Palette로 변경.
    비활성화 된 Mode의 아이콘을 Outlined로 표시하도록 변경.

--- src/lib/Layout/modeSwitch.tsx
+++ src/lib/Layout/modeSwitch.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import './modeSwitch.css'; // 스타일 파일 임포트
 import { Icon } from '@midasit-dev/moaui'; // 아이콘 라이브러리 임포트
-
 const Switch = (props: { setMode: any }) => {
 	const { setMode } = props;
 	const [isChecked, setIsChecked] = useState(false);
@@ -16,10 +15,10 @@ const Switch = (props: { setMode: any }) => {
 			<input type='checkbox' checked={isChecked} onChange={toggleSwitch} />
 			<span className='slider'></span>
 			<span className={isChecked ? 'layer' : 'checkedlayer'}>
-				<Icon iconName='Dashboard' /> {/* 'Dashboard' 아이콘은 오른쪽에 위치 */}
+				<Icon iconName={isChecked ? 'DashboardOutlined' : 'Dashboard'} /> {/* 'Dashboard' 아이콘은 오른쪽에 위치 */}
 			</span>
 			<span className={isChecked ? 'uncheckedcomponentized' : 'componentized'}>
-				<Icon iconName='Adjust' /> {/* 'Adjust' 아이콘은 왼쪽에 위치 */}
+				<Icon iconName={isChecked ? 'Palette' : 'PaletteOutlined'} /> {/* 'Adjust' 아이콘은 왼쪽에 위치 */}
 			</span>
 		</label>
 	);
