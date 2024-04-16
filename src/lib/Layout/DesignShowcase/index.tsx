86eed7a35ee5ef734bf3ee643c1495c37ade527f
lhy0118 <lhy0118@midasit.com>
2024-04-16 12:28:48 +0900

    배경 svg 컴포넌트 파일 생성 및 코드 분리

--- src/lib/Layout/DesignShowcase/index.tsx
+++ src/lib/Layout/DesignShowcase/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
 import ShowBox from './showbox';
+import ColorfulBg from './colorfulBg';
+
 export default function DesignShowcase() {
 	return (
 		<div
@@ -9,9 +12,38 @@ export default function DesignShowcase() {
 				display: 'flex',
 				justifyContent: 'center',
 				alignItems: 'center',
+				overflow: "hidden"
 			}}
 		>
-			<ShowBox />
+			<motion.div
+				initial={{ opacity: 0 }}
+				animate={{ opacity: 1 }}
+				exit={{ opacity: 0 }}
+				transition={{
+					duration: 0.5,
+					type: 'easeInOut',
+				}}
+			>
+				<AnimatePresence>
+					<motion.div
+						initial={{ opacity: 0 }}
+						animate={{ opacity: 1 }}
+						exit={{ opacity: 0 }}
+						transition={{ duration: 0.5, type: 'easeInOut' }}
+						style={{
+							position: 'absolute',
+							top: 0,
+							left: 0,
+							width: '100%',
+							height: '100%',
+							zIndex: -1,
+						}}
+					>
+						<ColorfulBg key='chat-login-background' />
+					</motion.div>
+				</AnimatePresence>
+				<ShowBox />
+			</motion.div>
 		</div>
 	);
 }
--- src/lib/Layout/DesignShowcase/showbox.tsx
+++ src/lib/Layout/DesignShowcase/showbox.tsx
@@ -1,10 +1,36 @@
 import React from 'react';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { motion, AnimatePresence } from 'framer-motion';
+
+import { CanvasState } from '../recoilState';
 
 export default function ShowBox() {
+	const canvasState = useRecoilValue(CanvasState);
 
 	return (
-		<div style={{width:"600px", height:'520px', borderRadius:"8px", backgroundColor:"#FFF"}}>
-			<p>Design Showcase is a component that shows the design of the component.</p>
-		</div>
-	)
+		<AnimatePresence mode='popLayout'>
+			<div
+				className='wrapper-box'
+				style={{
+					zIndex: 1,
+					width: canvasState.width,
+					height: canvasState.height,
+					maxWidth: '100%',
+					maxHeight: 'calc(100vh - 32px)',
+					boxSizing: 'border-box',
+					display: 'flex',
+					flexWrap: 'wrap',
+					alignContent: 'flex-start',
+					backgroundColor: '#ffffff',
+					boxShadow:
+						'var(--elevation-200-canvas, 0px 0px .5px rgba(0, 0, 0, .18), 0px 3px 8px rgba(0, 0, 0, .1), 0px 1px 3px rgba(0, 0, 0, .1))',
+					borderRadius: '0.25rem',
+					marginBottom: '3rem',
+					border: '1px solid #d1d1d1',
+				}}
+			>
+				<p>Design Showcase is a component that shows the design of the component.</p>
+			</div>
+		</AnimatePresence>
+	);
 }
