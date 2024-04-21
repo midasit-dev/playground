34bbecd0238454c96185089093b6ea7d8d8db5a9
lhy0118 <lhy0118@midasit.com>
2024-04-21 16:32:05 +0900

    Json, Code 옵션 임시로 사용가능하도록 변경 (Temp Code)

--- src/lib/Layout/Componentized/index.tsx
+++ src/lib/Layout/Componentized/index.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { useRecoilState } from 'recoil';
 import { CanvasState, LayersState } from '../recoilState';
 import { GuideBox, Panel, Separator } from '@midasit-dev/moaui';
@@ -9,11 +10,18 @@ import PanelOptionsModifyDelete from './PanelOptionsModifyDelete';
 import ToComponent from './ToComponent';
 import '../SelectedLayer.css';
 import { motion, AnimatePresence } from 'framer-motion'; // framer-motion 라이브러리를 임포트합니다.
+import JSONImport from '../JsonOptions/Import';
+import JSONExport from '../JsonOptions/Export';
 import JSONView from '../JsonOptions/View';
+import CODEOption from '../GenerateCode';
 
 const App = () => {
 	const [canvas] = useRecoilState(CanvasState);
 	const [layers] = useRecoilState(LayersState);
+	const [openJsonImportMenu, setOpenJsonImportMenu] = React.useState(false);
+	const [openJsonExportMenu, setOpenJsonExportMenu] = React.useState(false);
+	const [openJsonViewMenu, setOpenJsonViewMenu] = React.useState(false);
+	const [openCodeOption, setOpenCodeOption] = React.useState(false);
 
 	return (
 		<AnimatePresence>
@@ -24,8 +32,24 @@ const App = () => {
 				style={{ width: '100%', height: '100%' }}
 				className='bg-gray-100'
 			>
-				<JSONView />
-
+				{openJsonImportMenu && <JSONImport openJsonImportMenu={openJsonImportMenu} setOpenJsonImportMenu={setOpenJsonImportMenu}/>}
+				{openJsonExportMenu && <JSONExport openJsonExportMenu={openJsonExportMenu} setOpenJsonExportMenu={setOpenJsonExportMenu}/>}
+				{openJsonViewMenu && <JSONView />}
+				{openCodeOption && <CODEOption openCodeMenu={openCodeOption} setOpenCodeMenu={setOpenCodeOption}/>}
+				<div style={{display:'flex', flexDirection:"row", justifyContent:"end",}}>
+				<div style={{cursor:"pointer", marginRight:"10px"}} onClick={() => {setOpenJsonImportMenu(true)}}>
+					Import
+				</div>
+				<div style={{marginRight:"10px", cursor:"pointer"}} onClick={() => {setOpenJsonExportMenu(true)}}>
+					Export
+				</div>
+				<div style={{marginRight:"10px", cursor:"pointer"}} onClick={() => {setOpenJsonViewMenu(true)}}>
+					View
+				</div>
+				<div style={{marginRight:"10px", cursor:"pointer"}} onClick={() => {setOpenCodeOption(true)}}>
+					Code
+				</div>
+				</div>
 				<GuideBox row width='100%'>
 					{/** Canvas 객체 */}
 					<div className='w-full h-[calc(100vh-32px)] flex justify-center items-center'>
