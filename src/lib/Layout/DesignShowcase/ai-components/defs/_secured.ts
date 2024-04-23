96afbb064383b6e9de32bba029ce941c61c56251
lhy0118 <lhy0118@midasit.com>
2024-04-23 18:16:02 +0900

    secure 정보 원복

--- src/lib/Layout/DesignShowcase/ai-components/defs/_secured.ts
+++ src/lib/Layout/DesignShowcase/ai-components/defs/_secured.ts
@@ -1,13 +1,11 @@
-const authApiEndpoint: string = 'https://api-agw-backend.midasuser.com/auth/v1/login';
+const authApiEndpoint: string = '';
 const userIdentifier: string = JSON.stringify({
-	email: 'midas_official@midasit.com',
-	password: 'q1w2e3r4t5',
+	email: '',
+	password: '',
 });
 const getAiResponse = (value: string) =>
-	`https://moa.rpm.kr-dv-midasit.com/backend/gpt/function-knowledges/${value}/similar-functions`;
+	``;
 const getAiSchemaCode = (threadId: string | number, functionId: string | number) =>
-	`https://moa.rpm.kr-dv-midasit.com/backend/gpt/function-knowledges/${String(
-		threadId,
-	)}/functions/${String(functionId)}`;
+	``;
 
 export { authApiEndpoint, userIdentifier, getAiResponse, getAiSchemaCode };
